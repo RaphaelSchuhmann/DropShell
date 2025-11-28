ec51fde94737a52920b64de668e63fc836df02ad
Raphael Schuhmann <raphael221@outlook.de>
2025-11-28 14:56:49 +0100

    Added HotKey registration service

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using DropShell.Config;
+using DropShell.Services.Hotkey;
 using System.Windows;
 
 namespace DropShell
@@ -8,15 +9,28 @@ namespace DropShell
     /// </summary>
     public partial class MainWindow : Window
     {
+        private HotkeyService _hotkeyService;
+
         public MainWindow()
         {
             InitializeComponent();
+            _hotkeyService = new HotkeyService();
+
+            this.Loaded += (s, e) =>
+            {
+                _hotkeyService.Register(ConfigService.Instance.Config.HotKey, this);
+            };
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var hotkey = ConfigService.Instance.Config.HotKey;
-            MessageBox.Show(hotkey);
+            MessageBox.Show("Button clicked");
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            _hotkeyService.Unregister(this); // unregister the hotkey
+            base.OnClosing(e);
         }
     }
 }
