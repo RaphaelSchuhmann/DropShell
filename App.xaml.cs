4cda1306d3a9b86725af66d0fc7e351d4b0aebff
Raphael Schuhmann <raphael221@outlook.de>
2025-11-28 09:27:46 +0100

    Project setup

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DropShell
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
