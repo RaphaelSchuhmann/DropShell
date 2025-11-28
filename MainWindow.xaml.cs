6568569ef2ad4fdd1affdf89b96466f4d3e3bf53
Raphael Schuhmann <raphael221@outlook.de>
2025-11-28 13:30:15 +0100

    Implement configuration loading and main window setup

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
+﻿using DropShell.Config;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DropShell
 {
@@ -19,6 +11,12 @@ namespace DropShell
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var hotkey = ConfigService.Instance.Config.HotKey;
+            MessageBox.Show(hotkey);
         }
     }
 }
