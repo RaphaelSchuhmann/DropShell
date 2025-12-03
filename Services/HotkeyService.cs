ecfd1ca28efd9acbe6391b8dc74c26bc95cdb5d5
Raphael Schuhmann <raphael221@outlook.de>
2025-12-03 10:52:19 +0100

    Updated hotkey key map, Removed test button from main window

--- Services/HotkeyService.cs
+++ Services/HotkeyService.cs
@@ -22,40 +22,44 @@ namespace DropShell.Services.Hotkey
             { "q", 0x51 }, { "r", 0x52 }, { "s", 0x53 }, { "t", 0x54 },
             { "u", 0x55 }, { "v", 0x56 }, { "w", 0x57 }, { "x", 0x58 },
             { "y", 0x59 }, { "z", 0x5A },
-
-            // Numbers
+        
+            // Numbers (top row)
             { "0", 0x30 }, { "1", 0x31 }, { "2", 0x32 }, { "3", 0x33 },
             { "4", 0x34 }, { "5", 0x35 }, { "6", 0x36 }, { "7", 0x37 },
             { "8", 0x38 }, { "9", 0x39 },
-
+        
             // Function keys
-            { "F1", 0x70 }, { "F2", 0x71 }, { "F3", 0x72 }, { "F4", 0x73 },
-            { "F5", 0x74 }, { "F6", 0x75 }, { "F7", 0x76 }, { "F8", 0x77 },
-            { "F9", 0x78 }, { "F10", 0x79 }, { "F11", 0x7A }, { "F12", 0x7B },
-
+            { "f1", 0x70 }, { "f2", 0x71 }, { "f3", 0x72 }, { "f4", 0x73 },
+            { "f5", 0x74 }, { "f6", 0x75 }, { "f7", 0x76 }, { "f8", 0x77 },
+            { "f9", 0x78 }, { "f10", 0x79 }, { "f11", 0x7A }, { "f12", 0x7B },
+        
             // Arrows
             { "left", 0x25 }, { "up", 0x26 }, { "right", 0x27 }, { "down", 0x28 },
-
-            // Special keys
+        
+            // Control keys
             { "backspace", 0x08 }, { "tab", 0x09 }, { "enter", 0x0D },
             { "shift", 0x10 }, { "ctrl", 0x11 }, { "alt", 0x12 },
             { "pause", 0x13 }, { "capslock", 0x14 }, { "esc", 0x1B },
             { "space", 0x20 }, { "pageup", 0x21 }, { "pagedown", 0x22 },
-            { "end", 0x23 }, { "home", 0x24 }, { "insert", 0x2D }, { "DELETE", 0x2E },
-
-            // Numpad keys
+            { "end", 0x23 }, { "home", 0x24 }, { "insert", 0x2D }, { "delete", 0x2E },
+        
+            // Numpad
             { "num0", 0x60 }, { "num1", 0x61 }, { "num2", 0x62 }, { "num3", 0x63 },
             { "num4", 0x64 }, { "num5", 0x65 }, { "num6", 0x66 }, { "num7", 0x67 },
             { "num8", 0x68 }, { "num9", 0x69 },
-
             { "multiply", 0x6A }, { "add", 0x6B }, { "separator", 0x6C }, { "subtract", 0x6D },
             { "decimal", 0x6E }, { "divide", 0x6F },
-
-            // Other
-            { "oem_1", 0xBA }, { "oem_plus", 0xBB }, { "oem_comma", 0xBC }, { "oem_minus", 0xBD },
-            { "oem_period", 0xBE }, { "oem_2", 0xBF }, { "oem_3", 0xC0 },
+        
+            // German keyboard symbols (top row & OEM keys)
+            { "^", 0xC0 }, { "°", 0xDE }, { "+", 0xBB }, { "#", 0xBF }, { "ü", 0xDB }, { "ö", 0xDD }, { "ä", 0xDC }, 
+            { ",", 0xBC }, { ".", 0xBE }, { "-", 0xBD }, { ";", 0xBA }, { "'", 0xDE }, { "`", 0xC0 }, { "\\", 0xDC }, 
+            { "[", 0xDB }, { "]", 0xDD }, { "/", 0xBF }, 
+        
+            // Optional VK names (fallback)
+            { "oem_1", 0xBA }, { "oem_2", 0xBF }, { "oem_3", 0xC0 },
             { "oem_4", 0xDB }, { "oem_5", 0xDC }, { "oem_6", 0xDD }, { "oem_7", 0xDE }
         };
+
 
         public void Register(string hotKey, Window window)
         {
