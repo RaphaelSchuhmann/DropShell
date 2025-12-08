96f86661c16aedb2d0cfe8a3594985b314a26e11
Raphael Schuhmann <raphael221@outlook.de>
2025-12-08 13:57:44 +0100

    Remove doc comment

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,9 +11,6 @@ using System.Runtime.CompilerServices;
 
 namespace DropShell
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
